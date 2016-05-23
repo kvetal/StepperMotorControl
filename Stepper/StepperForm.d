5df71aeb3ff048eba168f2234bef2de6761ce82c
kvetal <kvetal@mail.ru>
2016-05-23 12:50:03 +0500

    Добавлена кнопка закрытия порта

--- Stepper/StepperForm.d
+++ Stepper/StepperForm.d
@@ -19,22 +19,28 @@ extern (C) {
 	void h_qsbDelay(StepperForm* h,int N,int val){
 		(*h).m_qrbValue(N,val);
 	}
+	void h_qpbClosePort(StepperForm* h){
+		(*h).m_qpbClosePort();
+
+	}
 }
 
 
 class StepperForm : QWidget
 {
-	QVBoxLayout vMainLayout; //vL1,vL2,
-	QHBoxLayout hL1,hL2,hL3,hL4;
-	QLabel labelPort, labelDelay, labelVLevel;
-	QLineEdit qlePort;
-	QSpinBox qsbDelay, qsbVLevel;
-	QRadioButton qrbCW,qrbCCW,qrbStop;
-	//	QCheckBox qcbOnOff;
-	SerialPort s_port;
-	char[] buf;
-	QAction act_CW,act_CCW,act_Stop, act_qsbDelay;
-
+	private {
+		QPushButton qpbClosePort;
+		QVBoxLayout vMainLayout; //vL1,vL2,
+		QHBoxLayout hL1,hL2,hL3,hL4;
+		QLabel labelPort, labelDelay, labelVLevel;
+		QLineEdit qlePort;
+		QSpinBox qsbDelay, qsbVLevel;
+		QRadioButton qrbCW,qrbCCW,qrbStop;
+		//	QCheckBox qcbOnOff;
+		SerialPort s_port;
+		char[] buf;
+		QAction act_CW,act_CCW,act_Stop, act_qsbDelay,act_qpbClosePort;
+	}
 	
 
 	this()
@@ -42,9 +48,11 @@ class StepperForm : QWidget
 		super(this);
 		setWindowTitle("Управление шаговым двигателем");
 		resize(300,100);
-		//		vL1 = new QVBoxLayout();
-		//		vL2 = new QVBoxLayout();
+
 		vMainLayout = new QVBoxLayout();
+		qpbClosePort = new QPushButton(this);
+		qpbClosePort.setText("Закрыть порт");
+		qpbClosePort.setEnabled(false);
 
 		hL1 = new QHBoxLayout();
 		hL2 = new QHBoxLayout();
@@ -56,8 +64,6 @@ class StepperForm : QWidget
 		labelPort.setText("Порт:");
 		labelDelay= new QLabel(this);
 		labelDelay.setText("Задержка между шагами (мс)");
-		//		labelVLevel = new QLabel(this);
-		//		labelVLevel.setText("Уровень напряжения между шагами \n(Меньше нагрузка на мотор) ");
 
 		qlePort = new QLineEdit(this);
 		qlePort.setText("/dev/ttyUSB0");
@@ -66,10 +72,6 @@ class StepperForm : QWidget
 		qsbDelay.setMaximumWidth(100);
 		qsbDelay.setMaximum(10000);
 		qsbDelay.setMinimum(1);
-		/*	qsbVLevel = new QSpinBox(this);
-		 qsbVLevel.setMaximumWidth(100);
-		 qsbVLevel.setMaximum(255);
-		 qsbVLevel.setMinimum(1);*/
 
 		qrbCW = new QRadioButton(this);
 		qrbCW.setText("CW");
@@ -79,16 +81,9 @@ class StepperForm : QWidget
 		qrbStop.setText("Stop");
 
 		
-
-		//		qcbOnOff = new QCheckBox(this);
-		//      qcbOnOff.setText("Вкл/Выкл");
-
-		hL1.addWidget(labelPort).addWidget(qlePort);
+		hL1.addWidget(labelPort).addWidget(qlePort).addWidget(qpbClosePort);
 		hL2.addWidget(labelDelay).addWidget(qsbDelay);
-		//		hL3.addWidget(labelVLevel).addWidget(qsbVLevel);
-		//		vL1.addWidget(qrbCW).addWidget(qrbCCW);
-		//		vL2.addWidget(qcbOnOff);
-		//		hL4.addLayout(vL1).addLayout(vL2);
+
 		hL4.addWidget(qrbCW).addWidget(qrbCCW).addWidget(qrbStop);
 		vMainLayout.addLayout(hL1).addLayout(hL2).addLayout(hL3).addLayout(hL4);
 		setLayout(vMainLayout);
@@ -97,11 +92,13 @@ class StepperForm : QWidget
 		act_CCW = new QAction(this,&h_qrbCCW,aThis);
 		act_Stop = new QAction(this,&h_qrbStop,aThis);
 		act_qsbDelay = new QAction(this,&h_qsbDelay,aThis);
+		act_qpbClosePort = new QAction(this,&h_qpbClosePort,aThis);
 
 		connects(qrbCW,"toggled(bool)",act_CW,"Slot_v__A_N_b(bool)");
 		connects(qrbCCW,"toggled(bool)",act_CCW,"Slot_v__A_N_b(bool)");
 		connects(qrbStop,"toggled(bool)",act_Stop,"Slot_v__A_N_b(bool)");
 		connects(qsbDelay,"valueChanged(int)",act_qsbDelay,"Slot_v__A_N_i(int)");
+		connects(qpbClosePort,"clicked()",act_qpbClosePort,"Slot()");
 
 		
 
@@ -117,17 +114,33 @@ class StepperForm : QWidget
 	 * "D" Следующие символы до "\0" переводятся в число тип int и задаются в качестве задержки между шагами.
 	 * 
 	 */
-	 
+	
+	void portOpen(){ 
+		if ((s_port is null)||(s_port.closed)) {
+			s_port = new SerialPort(qlePort.text!string); 
+			Thread.sleep(dur!("seconds")(2));
+		}
+		if ((s_port !is null)||(!s_port.closed))qpbClosePort.setEnabled(true);
+
+		
+	}
+
+	void m_qpbClosePort(){
+		s_port.close();
+		if ((s_port is null)||(s_port.closed)) {
+			qpbClosePort.setEnabled(false);
+			qlePort.setEnabled = true;
+
+		}
+	}
 
 	// при Изменении значения в qsbDelay меняем задержку между шагами и восстанавливаем действие.
 	void m_qrbValue(int N,int val){
 		try{
-			if (s_port is null) {
-				s_port = new SerialPort(qlePort.text!string);
-				Thread.sleep(dur!("seconds")(2));
-			}
+			portOpen();
+			if ((s_port !is null)||(!s_port.closed)) qlePort.setEnabled = false; //Блокировка поля ввода если порт открыт
 			string tmpstr;
-			if (qrbCW.isChecked) {
+			if (qrbCW.isChecked) {			
 				tmpstr = "D"~to!string(val)~"\0"~"F";
 				s_port.write(tmpstr);
 			}
@@ -151,10 +164,8 @@ class StepperForm : QWidget
 	// Крутим вперёд (По часовой)
 	void m_qrbCW(int N,bool sel){
 		try{
-			if (s_port is null) {
-				s_port = new SerialPort(qlePort.text!string);
-				Thread.sleep(dur!("seconds")(2));
-			}
+			portOpen();
+			if ((s_port !is null)||(!s_port.closed)) qlePort.setEnabled = false; //Блокировка поля ввода если порт открыт
 			if (qrbCW.isChecked) {
 				string tmpstr;
 				tmpstr = "D"~to!string(qsbDelay.value)~"\0"~"F";
@@ -169,10 +180,8 @@ class StepperForm : QWidget
 	// Крутим назад (Против часовой)
 	void m_qrbCCW(int N,bool sel){
 		try{
-			if (s_port is null) {
-				s_port = new SerialPort(qlePort.text!string);
-				Thread.sleep(dur!("seconds")(2));
-			}
+			portOpen();
+			if ((s_port !is null)||(!s_port.closed)) qlePort.setEnabled = false; //Блокировка поля ввода если порт открыт
 			if (qrbCCW.isChecked) {
 				string tmpstr;
 				tmpstr = "D"~to!string(qsbDelay.value)~"\0"~"B";
@@ -187,10 +196,8 @@ class StepperForm : QWidget
 	// Останавливаем (с отключением напряжения с обмоток)
 	void m_qrbStop(int N,bool sel){
 		try{
-			if (s_port is null) {
-				s_port = new SerialPort(qlePort.text!string);
-				Thread.sleep(dur!("seconds")(2));
-			}
+			portOpen();
+			if ((s_port !is null)||(!s_port.closed)) qlePort.setEnabled = false; //Блокировка поля ввода если порт открыт
 			if (qrbStop.isChecked) {
 				s_port.write("S");
 			}
